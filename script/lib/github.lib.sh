dfe5e601fe988be75e4d31865fc392a799797772
B. van Berkum <dev@dotmpe.com>
2020-09-13 19:48:04 +0200

    github.lib cleanup

--- script/lib/github.lib.sh
+++ script/lib/github.lib.sh
@@ -3,32 +3,33 @@
 
 github_lib_load()
 {
-  test -n "$STATUSDIR_ROOT" || STATUSDIR_ROOT=$HOME/.statusdir
-  test -n "$UCONFDIR" || UCONFDIR=$HOME/.conf
-  test -n "$UCACHEDIR" || UCACHEDIR=$HOME/.cache
+  test -n "${STATUSDIR_ROOT-}" || STATUSDIR_ROOT=$HOME/.statusdir/
+  test -n "${UCONFDIR-}" || UCONFDIR=$HOME/.conf
+  test -n "${UCACHEDIR-}" || UCACHEDIR=$HOME/.cache
 
   gh_cache=$UCACHEDIR/github-lib
 
-  # gh_repo_list_spec=$STATUSDIR_ROOT/tree/github/repos/\$username.list
-  gh_repo_lists=$STATUSDIR_ROOT/tree/github/repos
+  # gh_repo_list_spec=${STATUSDIR_ROOT}tree/github/repos/\$username.list
+  gh_repo_lists=${STATUSDIR_ROOT}tree/github/repos
 }
 
 github_lib_init()
 {
   test -d "$gh_cache" || mkdir -p "$gh_cache"
   test -d "$gh_repo_lists" || mkdir -p "$gh_repo_lists"
+
+  true "${HTD_GIT_REMOTE:="$NS_NAME"}"
 }
 
 
 github_repos_list() # [Username]
 {
-  test -n "$1" || set -- $NS_NAME
+  test -n "${1-}" || set -- $HTD_GIT_REMOTE
   test -n "$1" || return
 
   local cached=$gh_cache/$1/repos.json list=$gh_repo_lists/$1.list
 
-  { test -e $cached && newer_than $cached $_1DAY
-  #} && $LOG info github.lib "Cached list" "$cached" || {
+  { test -e $cached -a -s $cached && newer_than $cached $_1DAY
   } && std_info "Cached list <$cached $_1DAY>" || {
 
     URL="https://api.github.com/users/$1/repos"
@@ -36,10 +37,9 @@ github_repos_list() # [Username]
 
     test -d $gh_cache/$1 || mkdir $gh_cache/$1
 
-    htd_resolve_paged_json "$URL" per_page page >"$cached"|| return $?
+    web_resolve_paged_json "$URL" per_page page >"$cached"|| return $?
 
     std_info "Updated cache github user-repos.json <$cached>"
-    #$LOG info github.lib "Updated cache github user-repos.json" "$cached"
   }
 
   test -e $list -a $list -nt $cached && {
@@ -48,28 +48,7 @@ github_repos_list() # [Username]
     jq -r 'to_entries[] as $r | $r.value.full_name' $cached | tee $list
 
     std_info "Updated list from new cache <$cached $list>"
-    #$LOG info github.lib "Updated list from new cache" "$list"
   }
 
-  #$LOG note github.lib \
   note "$(count_lines "$list") (public) repositories listed for '$1' <$list>"
-}
-
-
-htd_github_default()
-{
-  true
-}
-
-htd_github_new_token() # [User-Id] Description
-{
-  test -n "$1" || set -- "bvberkum" "$2"
-  test -n "$2" || {
-    $LOG error gh-new-token "Description Id needed" "" 1
-    return 1
-  }
-  curl \
-    -u "$1" \
-    -d '{"scopes":["repo"], "note":"'"$2"'"}' \
-        https://api.github.com/authorizations
 }
