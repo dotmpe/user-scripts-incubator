ea05b929a36066f6b336bb5cf7d634cda2785177
B. van Berkum <dev@dotmpe.com>
2022-07-07 23:32:59 +0200

    Minor fix to load.*sh files

--- load.bash
+++ load.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 $INIT_LOG "note" "" "Adding SCRIPTPATH" "$(dirname "${BASH_SOURCE[0]}")"
-SCRIPTPATH="$SCRIPTPATH${SCRIPTPATH:+":"}$(dirname "${BASH_SOURCE[0]}")/script/lib"
+SCRIPTPATH="${SCRIPTPATH-}${SCRIPTPATH:+":"}$(dirname "${BASH_SOURCE[0]}")/script/lib"
