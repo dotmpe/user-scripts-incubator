5399ece83d5000953210eaa3ea5097c7eba656eb
B. van Berkum <dev@dotmpe.com>
2025-10-20 04:27:43 +0200

    Accumulated at t460s

--- script/lib/ctx-yt-dlp.lib.sh
+++ script/lib/ctx-yt-dlp.lib.sh
@@ -1,6 +1,8 @@
 ctx_yt_dlp_lib__load ()
 {
-  lib_require stattab context-class || return
+  lib_require stattab class-uc uc-class args &&
+  lib_init class-uc std-uc &&
+  class_init User/Context || return
 
   local yt_dlp_conf=${yt_dlp_conf:-yt-dlp-uc.tab}
   if_ok "${YT_DLP_CONF:=$(out_fmt= statusdir_lookup ${yt_dlp_conf:?} index)}" ||
@@ -14,49 +16,22 @@ ctx_yt_dlp_lib__init ()
   lib_require urlstat-class || return
 }
 
+# Handler for stattab-data-list reader
 # XXX: this only does YouTube URL's
-ctx_yt_dlp__downloads__update ()
+ctx_yt_dlp__downloads__fetch_infos ()
 {
-  local url dirdef=${DEF_DIR:-yt-dlp} lk=:@yt-dlp:downloads:update
+  local url lk=:@yt-dlp:downloads:update
   test "${stb_data:0:1}" = "#" && {
-    stderr echo "$stb_data stat=$stb_stat rest=$stb_rest"
+    stderr echo "DIRECTIVE: $stb_data stat=$stb_stat rest=$stb_rest"
     return
   }
+
   local url="$stb_data" yt_{id,mi,fbn,fext,tt,td,tbr,fps,vres,tl,fn,fp}
+  url_yt_dlp "$url"
+}
 
-  stderr echo "ctx_yt_dlp__downloads__update $# $*"
-
-  stderr declare -f class.Context
-  class.Context .field tag-refs "$stb_rest" &&
-  class.Context .load $ctx_tag_refs || return
-
-  TODO "@yt-dlp:downloads:update $(sys_caller)" || return
-
-  local ctx_id
-  Context.field id "$stb_rest" && {
-    test -n "${ctx_id-}" || {
-      echo Resource.id ""
-    }
-  }
-
-  stderr echo stb_rest=$stb_rest
-  stderr echo $ctx_urls.id_from_url "$url"
-  #$ctx_urls.exists &&
-  #$ctx_urls.fetch &&
-
-  url_yt_id "$url" || return
-  ctx_tab=$UC_DLTAB context --exists yt:$yt_id &&
-    $LOG notice "$lk" "Found ..." "$_" && return
-
-  url_yt_dlp "$url" || return
-  test -n "${yt_fp-}" || {
-    yt_fp=$(realpath "${yt_tl:?}") &&
-    url_yt_dlp_mi &&
-    url_yt_dl_fetch ||
-      return
-  }
-
-  local dir=${Dir:-${dirdef-}}
+ctx_yt_dlp__downloads__update ()
+{
   bytes=$(filesize "$yt_fp") &&
   yt_cat=$(jq -r '.categories[]' < "$yt_mi") || return
   yt_cat="${yt_cat:+${yt_cat//$'\n'/,}}"
@@ -66,7 +41,7 @@ ctx_yt_dlp__downloads__update ()
   if_ok "$(date_id $(date --iso=sec))" || return
   : "$_ - yt:$yt_id:"
   : "$_${Playlist:+ playlist:$Playlist}"
-  : "$_${dir:+ dir:$dir}"
+  : "$_${yt_cat:+ cat:$yt_cat}"
   : "$_${yt_tt:+ \`$yt_tt\`}"
   : "$_${yt_td:+ [$yt_td]}"
   : "$_${yt_vres:+ $yt_vres}"
@@ -79,87 +54,134 @@ ctx_yt_dlp__downloads__update ()
 
   echo "$_" >> "$UC_DLTAB" &&
   $LOG notice "$lk" "OK" "yt:$yt_id"
+
+  #stderr echo "ctx_yt_dlp__downloads__update $# $*"
+
+  #stderr declare -f class.Context
+  #class.Context .field tag-refs "$stb_rest" &&
+  #class.Context .load $ctx_tag_refs || return
+
+  #TODO "@yt-dlp:downloads:update $(sys_callers)" || return
+
+  local ctx_id
+  Context.field id "$stb_rest" && {
+    test -n "${ctx_id-}" || {
+      echo Resource.id ""
+    }
+  }
+
+  stderr echo stb_rest=$stb_rest
+  stderr echo $ctx_urls.id_from_url "$url"
+  #$ctx_urls.exists &&
+  #$ctx_urls.fetch &&
 }
 
 url_yt_id ()
 {
   local url=${1:?}
-  : "${url#*"watch?v="}"
-  : "${_%%&*}"
-  yt_id=$_
-  test "$url" != "$yt_id" ||
-    $LOG alert :url-yt "Unrecognized URL" "$url" 1 || return
+  : "${url#*"/watch?v="}"
+  : "${_%%"&"*}"
+  test "$url" != "$_" &&
+  yt_id=${_} &&
+  ((${#yt_id})) ||
+    $LOG alert :url-yt "Unrecognized URL" "$url" 1
 }
 
 url_yt_dlp_mi ()
 {
   yt_mi=.meta/info/yt--$yt_id.json
-  test -s "$yt_mi" || {
-    test ! -e "$yt_mi" || rm "$yt_mi" || return
-    $LOG debug :url-yt "Fetching meta" "$url"
-    yt-dlp -j "$url" > "$yt_mi" || exit $?
+  [[ -s "$yt_mi" ]] && {
+    true # >&2 echo "Found meta for ${url@Q}"
+  } || {
+    [[ ! -e "$yt_mi" ]] || rm "$yt_mi" || return
+    >&2 echo "Fetching meta for ${url@Q}"
+    #$LOG notice :url-yt "Fetching meta" "$url"
+    yt-dlp ${yt_dlp_opts-} -j "$url" > "$yt_mi" || exit $?
   }
 }
 
 url_yt_dlp () # ~ <URL>
 {
-  local url=${1:?} dirdef=${DEF_DIR:-yt-dlp}
-  url_yt_id "$url" &&
-  url_yt_dlp_mi || return
+  local url=${1:?}
+  url_yt_id "$url" || return
 
+  # See if download exists already in some format, use that as base resource.
+  # tl is tree-link, a symlink with simple name Id and some actual catalog name
+  # as target.
   # XXX: should later track entries (table) but create symlinks for now
-  local x
-  for x in .meta/tree/yt:$yt_id.*
-  do
-    test -e "$x" && {
-      test -s "$x" || break
-      yt_tl=$x
-      return # Return on first existing
+  : "$( shopt -s nullglob && echo .meta/tree/yt:$yt_id.* )" &&
+  : "${_%% *}" &&
+  yt_tl=$_ &&
+  true || return
+
+  #ctx_tab=$UC_DLTAB context --exists yt:$yt_id &&
+  #  $LOG notice "$lk" "Found ..." "$_" && return
+
+  # Download meta if we need to build a filename
+  [[ ${yt_fp:+set} ]] || {
+    [[ ${yt_tl:+set} && -e "${yt_tl-}" ]] && {
+      #>&2 echo "Symlink exists $yt_tl"
+      # Use symlink to get path instead
+      yt_fp=$(realpath "${yt_tl:?}") || return
+      yt_fn=${yt_fp##*/}
+    } || {
+      url_yt_dlp_mi &&
+      url_yt_dl_readinfo &&
+      url_yt_dl_path &&
+      true || return
     }
-  done
-
-  url_yt_dl_fetch &&
-  url_yt_dlp_path || return
-
-  set -- .meta/tree
-  test -n "${Dir-}" && set -- "$Dir"
-  mkdir -vp "$@"
-
-  test -s "$yt_fp" || {
-    test ! -e "$yt_fp" || rm "$yt_fp" || return
-    $LOG info : "Downloading" "$yt_fp"
-    yt-dlp -P "${Dir:-$dirdef}" -o "$yt_fn" "$url" || exit $?
   }
 
-  test ! -h "$yt_tl" || {
+  [[ -s "${yt_fp-}" ]] && {
+    ((ctx_yt_found+=1))
+    return
+    #stderr echo "Found $_ln/- ($ctx_yt_found) [#$yt_id] '$yt_fn'"
+  } || {
+    ((ctx_yt_pend+=1))
+    #stderr echo TODO $yt_id $yt_fn
+  }
+
+  : "${yt_cat:-yt-dlp}"
+
+  # remove empty
+  [[ ! -e "$yt_fp" ]] || rm "$yt_fp" || return
+
+  # put data at cat dir
+  $LOG notice : "Downloading" "$yt_fp"
+  yt-dlp ${yt_dlp_opts-} -P "$yt_cat" -o "$yt_fn" "$url" || exit $?
+
+  # Updated/create symlink
+  [[ ! -h "$yt_tl" ]] || {
     test "$_" = "../../$yt_fp" || rm -v "$yt_tl"
   }
-  test -h "$yt_tl" ||
+  [[ -h "$yt_tl" ]] ||
     ln -vs "../../$yt_fp" "$yt_tl" || return
 
   $LOG notice : "Done" "$url"
+  exit
 }
 
-url_yt_dl_fetch ()
+url_yt_dl_readinfo ()
 {
-  lines_vars yt_fbn yt_fext yt_tt yt_td yt_tbr yt_fps yt_vres \
-    <<< "$(< "$yt_mi" \
-      jq -r .filename,.ext,.title,.duration_string,.tbr,.fps,.resolution)"
+  if_ok "$(< "$yt_mi" \
+      jq -r .filename,.ext,.title,.duration_string,.tbr,.fps,.resolution
+    )" &&
+  <<< "$_" lines_vars yt_fbn yt_fext yt_tt yt_td yt_tbr yt_fps yt_vres ||
+    return
 
-  # The filename is probably more sanitized for uses as filename then title/fulltitle
+  # XXX: The filename field is probably more sanitized for uses as filename than title/fulltitle or is that optimistic
   : "${yt_fbn%.$yt_fext}"
   : "${_%"[$yt_id]"}"
   : "${_%% }"
   yt_fbn=$_
 }
 
-url_yt_dlp_path ()
+url_yt_dl_path ()
 {
-  #yt_of="%(title)s [%(id)s].%(ext)s"
-
   yt_tl=.meta/tree/yt:$yt_id.$yt_fext
-  yt_fn_old="$yt_tt [yt:$yt_id].$yt_fext"
+  #yt_fn_old="$yt_tt [yt:$yt_id].$yt_fext"
   yt_fn="$yt_fbn [$yt_td yt:$yt_id ${yt_tbr}bps ${yt_fps}fps ${yt_vres}].$yt_fext"
-  yt_fp="${Dir:-${dirdef:?}}/$yt_fn"
-
+  yt_fp="$yt_cat/$yt_fn"
 }
+
+# Id: us-inc ctx-yt-dlp.lib ex:ft=bash:
